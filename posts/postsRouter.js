764e486e4f862b06e191d2b8ded9cde448bfcbaa
David Horstman <davidzilla27@gmail.com>
2020-05-08 13:23:29 -0400

    GET /api/posts

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -2,6 +2,15 @@ const express = require('express');
 const db = require('../data/db.js');
 const router = express.Router();
 
+
+router.get("/", (req,res)=>{
+    db.find()
+    .then(posts => res.status(200).json(posts))
+    .catch(() => res.status(500).json(
+        { errorMessage: "The posts information could not be retrieved." }
+    ));
+});
+
 router.post("/", (req, res) => {
     const { title, contents } = req.body;
     if (!title || !contents) {
@@ -11,11 +20,8 @@ router.post("/", (req, res) => {
     }
     db
         .insert(req.body)
-        .then(({ id }) => db.findById(id)
-            .then(post => res.status(201).json(post))
-            .catch(() => res.status(500).json({
-                errorMessage: "There was an error while saving the post to the database"
-            })))
+        .then(({ id }) => db.findById(id))
+        .then(post => res.status(201).json(post))
         .catch(() => res.status(500).json({
             errorMessage: "There was an error while saving the post to the database"
         }));
